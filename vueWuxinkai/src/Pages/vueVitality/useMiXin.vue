99c598c2f2e97a49440ad221bb6a65d2d49b0411
xinkai.wu@tescomm.com.cn <xinkai.wu@tescomm.com.cn>
2019-02-21 21:49:05 +0800

    4

--- vueWuxinkai/src/Pages/vueVitality/useMiXin.vue
+++ vueWuxinkai/src/Pages/vueVitality/useMiXin.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <h1>1 在你已经写好了构造器后，需要增加方法或者临时的活动时使用的方法，这时用混入会减少源代码的污染。</h1>
+    <p>电商促销：临时需要</p>
+    <h1>2 很多地方都会用到的公用方法，用混入的方法可以减少代码量，实现代码重用。</h1>
     <el-button @click="helloMixin">helloMixin调用minxin中的方法</el-button>
     <el-button @click="callMixin">callMixin调用minxin中的方法</el-button>
   </div>
